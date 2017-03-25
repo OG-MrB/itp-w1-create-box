08ced55d0aedef99168fc0ac7e6a58da33e91de4
Eric <efletcher001@gmail.com>
2017-03-25 17:00:37 +0000

    Emily & Eric's project

--- create_box/main.py
+++ create_box/main.py
@@ -2,8 +2,39 @@
 
 
 def create_box(height, width, character):
-    pass
-
-
+    line = character * width
+    num1 = 0
+    box = ''
+    
+    
+    if height < 1 or width < 1:
+        return 'invalid'
+    while num1 < height:
+        box += line + '\n'
+        num1 += 1 
+    return box
+    
+    
 if __name__ == '__main__':
     create_box(3, 4, '*')
+
+
+
+
+
+def create_outline(height, width, character):
+
+    num1 = 0
+    top = character * width
+    bottom = character * width
+    sides = (width - (width - 2)) * character
+    while num1 < height:
+        if num1 == 0:
+            print top
+        elif  num1 > 1 and num1 < height:
+            print sides
+        else:
+            print bottom
+        num1 += 1
+
+create_outline(8,8,'^')
--- tests/test_main.py
+++ tests/test_main.py
@@ -18,6 +18,10 @@ xxxxxxxxxxxxxxxxxxxxxxxx
 xxxxxxxxxxxxxxxxxxxxxxxx
 """.lstrip()
 
+fourth_box_expected = """
+'invalid'
+""".lstrip()
+
 
 class TestCreateBox(unittest.TestCase):
     def test_box(self):
@@ -25,5 +29,13 @@ class TestCreateBox(unittest.TestCase):
 
     def test_small_box(self):
         self.assertEqual(create_box(1, 1, '@'), second_box_expected)
+        
+    def test_large_box(self):
+        self.assertEqual(create_box(3, 24, 'x'), third_box_expected)
 
+    def test_zero_width(self):
+        self.assertEqual(create_box(2, 0, '0'), fourth_box_expected)
+        
+    def test_zero_height(self):
+        self.assertEqual(create_box(0, 3, '!'), fourth_box_expected)
     # Add your own test using third_box_expected
